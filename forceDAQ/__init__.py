2474b7a1b04d7a5d6f99d19f5c30315efecee294
Oliver Lindemann <lindemann09@googlemail.com>
2015-01-08 17:33:27 +0100

    Revert "Revert "restructure""
    This reverts commit 534506b5eb099360fbd156bdc78eb4fd5adca1b5.

--- forceDAQ/__init__.py
+++ forceDAQ/__init__.py
@@ -1,8 +1,2 @@
 __version__ = "0.3"
 __author__ = "Oliver Lindemann"
-
-from clock import Clock
-import udp_connection
-import pyATIDAQ
-import force_sensor
-from data_recorder import DataRecorder, SoftTrigger, UDPData, ForceData
