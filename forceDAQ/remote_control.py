ad8545c7abac0534b77a641384eb1afcd1ca3218
Oliver Lindemann <lindemann09@googlemail.com>
2015-01-21 17:31:33 +0100

    bugfix

--- forceDAQ/remote_control.py
+++ forceDAQ/remote_control.py
@@ -6,4 +6,4 @@ class GUIRemoteControlCommands(object):
     COMMAND_STR = "$cmd"
     FEEDBACK, START, PAUSE, QUIT, THRESHOLDS, PICKLED_VALUE, FILENAME, \
     GET_FX, GET_FY, GET_FZ, GET_TX, GET_TY, GET_TZ \
-    = map(lambda x: "$cmd" + str(x), range(12))
+    = map(lambda x: "$cmd" + str(x), range(13))
