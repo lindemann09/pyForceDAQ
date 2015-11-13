84a46d97eb85357c337631baf71b56c45978f379
Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>
2015-11-13 15:33:15 +0100

    Version 0.6.2

--- forceDAQ_GUI.py
+++ forceDAQ_GUI.py
@@ -5,7 +5,7 @@ from forceDAQ import remote_control
 if __name__ == "__main__":
     from forceDAQ import gui
 
-    remote_control = False
+    remote_control = True
     gui.start(remote_control=remote_control,
               ask_filename=not remote_control,
               calibration_file="FT_demo.cal",
