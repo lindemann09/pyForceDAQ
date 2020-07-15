18d91190deb19c3f49f6a0ff1d0c6ef96c985441
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2020-07-15 19:11:42 +0200

    ips for linux

--- forceDAQ/base/udp_connection.py
+++ forceDAQ/base/udp_connection.py
@@ -16,41 +16,18 @@ from time import sleep, time
 from ..base.types import UDPData
 from .timer import Timer, get_time
 from .misc import PYTHON3
-if os.name != "nt":
-    import fcntl
-    import struct
-
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        if PYTHON3:
-            ifname = ifname.encode()
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
-                                            0x8915, struct.pack('256s',
-                                                    ifname[:15]))[20:24])
-
 
 def get_lan_ip():
-    # code bas on http://stackoverflow.com/questions/11735821/python-get-localhost-ip
-    ip = socket.gethostbyname(socket.gethostname())
-    if ip.startswith("127.") and os.name != "nt":
-        interfaces = [
-            "eth0",
-            "eth1",
-            "eth2",
-            "wlan0",
-            "wlan1",
-            "wifi0",
-            "ath0",
-            "ath1",
-            "ppp0",
-        ]
-        for ifname in interfaces:
-            try:
-                ip = get_interface_ip(ifname)
-                break
-            except IOError:
-                pass
-    return ip
+    if os.name != "nt":
+        # linux
+        from subprocess import check_output
+        rtn = check_output("hostname -I".split(" ")).split(" ")
+        return rtn[0].strip()
+
+    else:
+        # windows
+        # code bas on http://stackoverflow.com/questions/11735821/python-get-localhost-ip
+        return socket.gethostbyname(socket.gethostname())
 
 
 class UDPConnection(object):
