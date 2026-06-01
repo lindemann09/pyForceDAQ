cc851118f9deb10a4a69b923ead0d89fb1acdbbd
Oliver Lindemann <lindemann@essb.eur.nl>
2026-06-01 14:19:37 +0200

    minor

--- src/pyforcedaq/force.py
+++ src/pyforcedaq/force.py
@@ -6,6 +6,6 @@ program your force sensor
 __author__ = 'Oliver Lindemann'
 
 from ._lib.data_recorder import DataRecorder
-from ._lib.sensor import Sensor, SensorSettings
+from ._lib.sensor import Sensor
 from ._lib.sensor_process import SensorProcess
 from ._lib.settings import AppSettings, RecordingSettings, SensorSettings
